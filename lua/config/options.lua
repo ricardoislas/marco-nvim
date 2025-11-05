2bfcc14cfcc1604853feb5140a423d2c64e09e59
ricardoislas <ricardo.islas.ruiz@gmail.com>
2025-11-04 18:34:31 -0700

    [options.lua]: Disable all animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,6 @@
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 vim.g.lazyvim_picker = "snacks"
+
+-- Disable all animations
+vim.g.snacks_animate = false
