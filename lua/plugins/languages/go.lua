4d89ec77d1ef5235ada027b14bd6202d36e241ab
ricardoislas <ricardo.islas.ruiz@gmail.com>
2026-01-07 23:08:52 -0700

    add basic go support

---
+++ lua/plugins/languages/go.lua
@@ -0,0 +1,102 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
+  },
+  -- Ensure Go tools are installed
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "golangci-lint",
+        "delve",
+      },
+    },
+  },
+  ---
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+      setup = {
+        ---@diagnostic disable-next-line: unused-local
+        gopls = function(_, opts)
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          Snacks.util.lsp.on({ name = "gopls" }, function(_, client)
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  ---@diagnostic disable-next-line: need-check-nil
+                  tokenTypes = semantic.tokenTypes,
+                  ---@diagnostic disable-next-line: need-check-nil
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end)
+          -- end workaround
+        end,
+      },
+    },
+  },
+  {
+    "fredrikaverpil/neotest-golang",
+  },
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      file = {
+        [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+      filetype = {
+        gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+      },
+    },
+  },
+}
